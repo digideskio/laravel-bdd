38354342e368c8b113a6ac05e2b6a84b494e67bc
SimoTod <simo.todaro@gmail.com>
2016-02-07 23:53:21 +0000

    add Service Provider

---
+++ src/Providers/BddServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+namespace SimoTod\LaravelBdd\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class BddServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../assets/features' => base_path('features'),
+            __DIR__.'/../assets/config' => base_path(''),
+        ]);
+    }
+}
